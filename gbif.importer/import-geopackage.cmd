950b30ec0548435ea99cdc5050f516575c1d6055
Energium <E-nergium@outlook.com>
2025-02-08 07:00:31 +0100

    https://github.com/Geonovius/Stikstof-2.0/issues/64

--- gbif.importer/import-geopackage.cmd
+++ gbif.importer/import-geopackage.cmd
@@ -1,1 +1,6 @@
+setx GDAL_DATA "c:\OSGeo4W\apps\gdal\share\gdal\"
+setx GDAL_DRIVER_PATH "c:\OSGeo4W\apps\gdal\lib\gdalplugins\"
+setx PROJ_LIB "c:\OSGeo4W\share\proj\"
+setx PYTHONPATH "c:\OSGeo4W\apps\Python312\"
+
 c:\OSGeo4W\bin\ogr2ogr.exe -f GPKG "d:\projects\gbifdata\0007494-250127130748423.gpkg" "d:\projects\gbifdata\0007494-250127130748423.csv" -progress -skipfailures -nlt NONE 
