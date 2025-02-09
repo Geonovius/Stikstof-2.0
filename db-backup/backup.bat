6f3bc27e2a0e89aff814a0257f50312841398ca5
Energium <E-nergium@outlook.com>
2025-02-09 23:35:12 +0100

    https://github.com/Geonovius/Stikstof-2.0/issues/67#issuecomment-2646631148

--- db-backup/backup.bat
+++ db-backup/backup.bat
@@ -1,2 +1,1 @@
-D:\PostgreSQL\17\pgAdmin 4\runtime\pg_dump.exe --file "D:\backup\stikstof.backup" --host "localhost" --port "5432" --username "postgres" --no-password --format=c --large-objects --section=pre-data --section=data --section=post-data --verbose --schema ""N2000"" --schema "aerius" --schema "krd" --schema "public" --schema ""rivm-dash"" "stikstof"
-
+"D:\PostgreSQL\17\pgAdmin 4\runtime\pg_dump.exe" --file "D:\backup\stikstof.backup" --dbname=postgresql://postgres:system@localhost:5432/stikstof --format=c --large-objects --section=pre-data --section=data --section=post-data --verbose --schema ""N2000"" --schema "aerius" --schema "krd" --schema "public" --schema ""rivm-dash""
