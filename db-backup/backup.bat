8d7cf92a87dca4fb752faa3e5f90b3a14314c68d
Energium <E-nergium@outlook.com>
2025-03-18 01:11:38 +0100

    updated db backup per datum voor alle schemas in de database

--- db-backup/backup.bat
+++ db-backup/backup.bat
@@ -1,1 +1,10 @@
-"D:\PostgreSQL\17\pgAdmin 4\runtime\pg_dump.exe" --file "D:\backup\stikstof.backup" --dbname=postgresql://postgres:system@localhost:5432/stikstof --format=c --large-objects --section=pre-data --section=data --section=post-data --verbose --schema ""N2000"" --schema "aerius" --schema "krd" --schema "public" --schema ""rivm-dash""
+REM DATETIME
+for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set DateTime=%%a
+
+set year=%DateTime:~0,4%
+set month=%DateTime:~4,2%
+set day=%DateTime:~6,2%
+
+set complete_datetime=%year%-%month%-%day%
+echo %complete_datetime%
+"D:\PostgreSQL\17\pgAdmin 4\runtime\pg_dump.exe" --file "D:\backup\%complete_datetime%_stikstof.backup" --dbname=postgresql://postgres:system@localhost:5432/stikstof --format=c --large-objects --section=pre-data --section=data --section=post-data --verbose 
