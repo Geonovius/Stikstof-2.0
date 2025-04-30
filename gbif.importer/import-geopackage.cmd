cce188a8d99e198906b1823086eef281641af96b
Energium <E-nergium@outlook.com>
2025-04-30 02:50:17 +0200

    https://github.com/Geonovius/Stikstof-2.0/issues/156#issuecomment-2840546658

--- gbif.importer/import-geopackage.cmd
+++ gbif.importer/import-geopackage.cmd
@@ -1,6 +1,7 @@
-setx GDAL_DATA "d:\projects\Stikstof-2.0\gdal\bin\gdal-data"
-setx GDAL_DRIVER_PATH "d:\projects\Stikstof-2.0\gdal\bin\gdal\plugins\"
-setx PROJ_LIB "d:\projects\Stikstof-2.0\gdal\bin\proj9\share\"
-setx PYTHONPATH "d:\projects\Stikstof-2.0\gdal\bin\gdal\"
+setx GDAL_DATA "d:\GDAL\gdal-data\"
+setx GDAL_DRIVER_PATH "d:\GDAL\gdalplugins\"
+setx PROJ_LIB "d:\GDAL\projlib\"
+setx PYTHONPATH "d:\GDAL\"
 
-d:\projects\Stikstof-2.0\gdal\bin\gdal\apps\ogr2ogr.exe -f GPKG "d:\projects\gbifdata\0007494-250127130748423.gpkg" "d:\projects\gbifdata\0007494-250127130748423.csv" -progress -skipfailures -nlt NONE 
+REM d:\GDAL\ogr2ogr.exe -f GPKG "d:\projects\gbifdata\t.gpkg" "d:\projects\gbifdata\0007494-250127130748423.csv" -progress -skipfailures -nlt NONE 
+d:\GDAL\ogr2ogr.exe --config PG_USE_COPY YES -f "PostgreSQL" PG:"dbname='stikstof' host='localhost' port='5432' user='postgres' password='system'" -nln gbif.occurencies "d:\projects\gbifdata\0001329-250426092105405\0001329-250426092105405.csv" -progress -skipfailures -nlt point -a_srs EPSG:28992
